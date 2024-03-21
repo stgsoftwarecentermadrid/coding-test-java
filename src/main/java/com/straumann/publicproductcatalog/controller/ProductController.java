9b90bb77f0f606b09307189e67bdee5af202e9d1
Diego Fernandez <u126530@straumann.com>
2024-03-21 12:17:42 +0100

    feat(public-product-catalog): Modify Product Controller.

--- src/main/java/com/straumann/publicproductcatalog/controller/ProductController.java
+++ src/main/java/com/straumann/publicproductcatalog/controller/ProductController.java
@@ -3,23 +3,13 @@ package com.straumann.publicproductcatalog.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ProductController {
 
     //TODO añade url del endpoint
-    public ResponseEntity<List<QualityAd>> qualityListing() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
-    }
-
-    //TODO añade url del endpoint
-    public ResponseEntity<List<PublicAd>> publicListing() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
-    }
-
-    //TODO añade url del endpoint
-    public ResponseEntity<Void> calculateScore() {
+    public ResponseEntity<List<ProductDTO>> publicProductsListing() {
         //TODO rellena el cuerpo del método
         return ResponseEntity.notFound().build();
     }
